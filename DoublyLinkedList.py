c03fe82580ad5b7038c5c4ebfb97c8104f6f4666
Shreyansh <shreyansh.sharan@gmail.com>
2022-04-08 12:28:24 +0530

    Added Insert_After and Insert_Before

--- DoublyLinkedList.py
+++ DoublyLinkedList.py
@@ -12,8 +12,6 @@ class DoubleLinkedList:
         if self.head is None:
             new_node = Node(data)
             self.head = new_node
-            self.next = new_node.next
-            self.prev = new_node.prev
             return
         else:
             cur_node = self.head
@@ -37,8 +35,47 @@ class DoubleLinkedList:
             self.head.prev = new_node
             self.head = new_node
         
+    def insert_before(self,existingVal,data):
+        cur_node = self.head
+        while cur_node:
+            if cur_node.prev is None and cur_node.data == existingVal:
+                self.prepend(data)
+                return
             
+            elif cur_node.data == existingVal:
+                new_node = Node(data)
+                
+                prev = cur_node.prev
+                cur_node.prev = new_node
+                prev.next = new_node
+                
+                new_node.prev = prev
+                new_node.next = cur_node
+    
             
+            cur_node = cur_node.next
+    def insert_after(self,existingVal,data):
+        
+        if self.head is None:
+            self.head = Node(data)
+
+        else:
+            cur_node = self.head
+            new_node = Node(data)
+            while cur_node.data != existingVal:
+                cur_node = cur_node.next
+
+            nxt = cur_node.next
+            if nxt is None:
+                self.append(data)
+                return
+            cur_node.next = new_node
+            new_node.next = nxt
+            nxt = new_node.next
+                    
+            nxt.prev = new_node
+        
+
     def print_list(self):
         
         cur_node = self.head
@@ -54,11 +91,10 @@ llist1.append(5)
 llist1.append(7)
 llist1.append(9)
 llist1.append(10)
-llist1.prepend(9)
-llist1.prepend(7)
-llist1.prepend(5)
-llist1.prepend(3)
-llist1.prepend(1)
+
+
+llist1.insert_after(9, 2)
+llist1.insert_before(10,2)
 
 
 
